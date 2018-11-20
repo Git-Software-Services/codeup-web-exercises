0681f525c57bbcea7233258c21c2ab64e77c6fc4
Dorian Wallace <dwallace@Dorians-MacBook-Pro.local>
2018-11-20 14:49:35 -0500

    pushing up js file

---
+++ js/functions.js
@@ -0,0 +1,124 @@
+"use strict";
+
+/**
+ * TODO:
+ * Create a function called 'sayHello' that takes a parameter 'name'.
+ * When called, the function should return a message that says hello to the passed in name.
+ *
+ * Example
+ * > sayHello("codeup") // returns "Hello, codeup!"
+ */
+
+function sayHello(name) {
+    return "hello " + name + "!";
+}
+// console.log(sayHello("Dorian"));
+
+/**
+ * TODO:
+ * Call the function 'sayHello' and pass your name as a string literal argument.
+ * Store the result of the function call in a variable named 'helloMessage'.
+ *
+ * console.log 'helloMessage' to check your work
+ */
+
+// var helloMessage = sayHello("Dorian");
+// console.log(helloMessage);
+
+/**
+ * TODO:
+ * Store your name as a string in a variable named 'myName', and pass that
+ * variable to the 'sayHello' function. You should see the same output in the
+ * console.
+ */
+
+// var myName = "Dorian";
+// sayHello(myName);
+// console.log(sayHello(myName));
+
+// Don't modify the following line, it generates a random number between 1 and 3
+// and stores it in a variable named random
+var random = Math.floor((Math.random() * 3) + 1);
+
+/**
+ * TODO:
+ * Create a function called 'isTwo' that takes a number as a parameter.
+ * The function should return a boolean value based on whether or not the passed
+ * number is the number 2.
+ *
+ * Example
+ * > isTwo(1) // returns false
+ * > isTwo(2) // returns true
+ * > isTwo(3) // returns false
+ *
+ * Call the function 'isTwo' passing the variable 'random' as a argument.
+ *
+ * console.log *outside of the function* to check your work (you should see a
+ * different result everytime you refresh the page if you are using the random
+ * number)
+ */
+// function isTwo(num) {
+//     if(num === 2) {
+//         return true;
+//     }else{return false;}
+// }
+// console.log(isTwo(random));
+
+
+// function isTwo(number) {
+//     return number === 2;
+//     console.log("the random number is " + random);
+// }
+/**
+ * TODO:
+ * Create a function named 'calculateTip' to calculate a tip on a bill at a
+ * restaurant. The function should accept a tip percentage and the total of the
+ * bill, and return the amount to tip
+ *
+ * Examples:
+ * > calculateTip(0.20, 20) // returns 4
+ * > calculateTip(0.25, 25.50) // returns 6.37
+ * > calculateTip(0.15, 33.42) // returns 5.013
+ */
+
+function calculateTip(percentage, total)  {
+    return total * percentage;
+}
+console.log(calculateTip(20, 0.20));
+console.log(calculateTip(25.50, 0.25));
+console.log(calculateTip(33.42, 0.15));
+/**
+ * TODO:
+ * Use prompt and alert in combination with your calculateTip function to
+ * prompt the user for the bill total and a percentage they would like to tip,
+ * then display the dollar amount they should tip
+ */
+
+// var total = parseFloat(prompt("What is your bill total?"));
+// var percentage = parseFloat(prompt("What percentage would you like to tip? Ex. for 20% use .20"));
+// alert("The amount you should tip is $" + calculateTip(percentage, total).toFixed(2));
+
+/**
+ * TODO:
+ * Create a function named `applyDiscount`. This function should accept a price
+ * (before a discount is applied), and a discount percentage (a number between 0
+ * and 1). It should return the result of applying the discount to the original
+ * price.
+ *
+ * Example:
+ * > var originalPrice = 100;
+ * > var dicountPercent = .2; // 20%
+ * > applyDiscount(originalPrice, dicountPercent) // 80
+ *
+ * > applyDiscount(45.99, 0.12) // 40.4712
+ */
+
+function applyDiscount(subtotal, discountPercentage) {
+    var discount = subtotal * discountPercentage;
+    console.log("Your discount percentage is " + discount + "%");
+    var total = subtotal - discount;
+    console.log("your total is $" + total);
+    return total;
+
+}
+applyDiscount(100, .1);
