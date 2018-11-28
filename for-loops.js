3d96321c1169da05f25688fa889e6a067709bba5
Dorian Wallace <dwallace@Dorians-MacBook-Pro.local>
2018-11-28 15:51:31 -0500

    adding for loops js file

---
+++ for-loops.js
@@ -0,0 +1,35 @@
+"use strict"
+// function showMultiplicationTable(input) {
+//     for (var num = 10; num > 0; num--) {
+//         var total = input * num;
+//         console.log(`${input} * ${num} = ${total}`);
+//     }
+// }
+// showMultiplicationTable(7);
+//
+// for (let counter = 1; counter < 11; counter++) {
+//     let randomNum = Math.floor(Math.random() * 180) + 20;
+//     if (randomNum % 2 === 0) {
+//         console.log(`${randomNum} is even.`);
+//     }else {console.log(`${randomNum} is odd.`);
+//     }
+// }
+//
+// for (let i = 1; i < 10; i++) {
+//     console.log(String(i).repeat(i))
+// }
+// function numberTree(){
+//     for (let i = 0; i < 10; i++) {
+//         let it = "";
+//         for (let j =0; j< i; j++) {
+//             it = it + i;
+//         } console.log(it);
+//     }
+// }
+//
+// numberTree()
+
+// for (let j = 100; j > 4; j -=5) {
+//     console.log(j);
+// }
+
