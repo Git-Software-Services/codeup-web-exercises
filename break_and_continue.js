cb89e58317ab6c9f01a7032e3ea9847db8aa8c2a
Dorian Wallace <dwallace@Dorians-MacBook-Pro.local>
2018-11-29 10:31:34 -0500

    added break and continue js file

---
+++ break_and_continue.js
@@ -0,0 +1,29 @@
+"use strict"
+
+
+function isNumeric (num) {
+    return (!isNaN(parseFloat(num)))
+}
+
+let userResponse ;
+do {
+    userResponse = parseFloat(prompt("Choose a number between 1 and 50."));
+//Keep asking until answer is good
+
+    if (isNumeric(userResponse)
+        && userResponse % 2 === 1
+        && userResponse >= 1
+        && userResponse <= 50)
+        break;
+}while(true)
+        console.log(userResponse);
+
+    for (var i = 1; i <= 50; i += 2) {
+        if (i === userResponse) {
+            console.log(`Yikes! Skipping the number: ${i}`);
+            continue;
+        }
+        console.log(`Here is an odd number: ${i}`)
+    }
+
+
