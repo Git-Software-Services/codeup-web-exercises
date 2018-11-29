b614f07bfe7f2d50277729e3164011566b9b4bad
Dorian Wallace <dwallace@Dorians-MacBook-Pro.local>
2018-11-29 18:33:07 -0500

    added planets array file

---
+++ planets-string.js
@@ -0,0 +1,43 @@
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray;
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */planetsArray = planetsString.split("|");
+
+    console.log(planetsArray);
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     *
+     * BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
+
+        // Replace ',' with '<br>'
+    let stringReplaceFrom = /,/g;
+    let stringReplaceTo = '<br>';
+    let planetsJoin = planetsArray.join(',').replace(stringReplaceFrom,stringReplaceTo).split();
+    console.log(planetsJoin);
+
+    console.log(planetsJoin.unshift());
+
+
+
+// pass a function to map
+    let mapPlanetsJoin = planetsJoin.map(unshift('<li>'));
+
+    console.log(mapPlanetsJoin);
+// expected output: Array [2, 8, 18, 32]
+
+})();
