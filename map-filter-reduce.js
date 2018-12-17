3d0118175263fee996e2cb6b146a274ed6e1907b
Your Name <dorian.l.wallace@gmail.com>
2018-12-17 18:51:51 -0500

    added map filter reduce exercise

---
+++ map-filter-reduce.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const userMultipleLanguages = users.filter(function(user){
+    return user.languages.length > 2
+
+});
+
+const userEmailAddresses = users.map(function(userEmail){
+    return userEmail.email;
+});
+
+const userYearlyAverage = users.reduce(function(counter, user)  {
+    return counter + user.yearsOfExperience / users.length;
+}, 0);
+
+const longestUserEmail = users.reduce(function(longestEmail, user) {
+    if (longestEmail.length < user.email.length){
+        return user.email;
+    }return longestEmail;
+}, '');
+
+var instructors = users.reduce(function(counter, instructor){
+    return counter + ` ${instructor.name}, `;
+}, '');
